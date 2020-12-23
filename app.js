8e385ebb7db92b24007002a28f1e48520bc990a1
Sylvie-B <unsolo@hotmail.fr>
2020-12-23 10:35:50 +0100

    recursive

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ let newDiv = document.createElement('div');
 let t = 0;
 
 function count () {
-    setInterval(function () {
-        if(t < 10){
-        t ++;
-        console.log("log 1 : " + t);
-        newDiv.innerHTML = t.toString();
-        document.body.appendChild(newDiv);
-        }
-    }, 1000)
+    if(t < 10) {
+        setTimeout(function () {
+            t++;
+            newDiv.innerHTML = t.toString();
+            document.body.appendChild(newDiv);
+            count();
+        }, 1000);
+    }
 }
 
 count();
